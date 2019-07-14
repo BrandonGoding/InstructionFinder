e4d3bcd51a9beffeea29add0ed605e402302d909
Robert Marinho <robstermarinho@gmail.com>
2019-07-14 16:43:43 -0300

    Sessions and Seats (Mongo Models)

--- instruction_finder/mongo_models.py
+++ instruction_finder/mongo_models.py
@@ -2,20 +2,28 @@ import datetime
 import mongoengine
 from instructionApp.settings import MONGODB_NAME, MONGODB_USER,MONGODB_PWD
 
-mongoengine.connect(MONGODB_NAME,
-                    username=MONGODB_USER,
-                    password=MONGODB_PWD,
-                    authentication_source='admin')
 
 # All models in this file are handled by Mongo
+class Seat(mongoengine.EmbeddedDocument):
+    student_id = mongoengine.IntField(required=True)
+    student_name = mongoengine.StringField()
+    amount_paid = mongoengine.FloatField()
+    date_paid = mongoengine.DateTimeField(default=datetime.datetime.now)
 
-class Course(mongoengine.Document):
-    title = mongoengine.StringField(required=True)
-    description = mongoengine.StringField(required=True)
+class Session(mongoengine.Document):
+    course_id = mongoengine.IntField(required=True)
+    course_title = mongoengine.StringField()
     instructor_id = mongoengine.IntField(required=True)
-    is_active = mongoengine.BooleanField(required=True, default=True)
+    instructor_name = mongoengine.StringField()
+    session_date = mongoengine.DateTimeField()
+    session_minutes_length = mongoengine.IntField(required=True)
+    session_price = mongoengine.FloatField()
+    session_currency = mongoengine.StringField(required=True)
     created_at = mongoengine.DateTimeField(default=datetime.datetime.now)
+    updated_at = mongoengine.DateTimeField(default=datetime.datetime.now)
 
-    meta = {
-        'collection': 'courses'
-    }
+    seats = mongoengine.EmbeddedDocumentListField(Seat)
+
+    def save(self, *args, **kwargs):
+        self.modified_date = datetime.datetime.now()
+        return super(Session, self).save(*args, **kwargs)
