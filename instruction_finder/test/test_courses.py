f46e2b57cfc1f5245e12d8a60c316ca22bef7892
Robert Marinho <robstermarinho@gmail.com>
2019-09-15 22:31:25 -0300

    Update courseAttributes

--- instruction_finder/test/test_courses.py
+++ instruction_finder/test/test_courses.py
@@ -39,20 +39,12 @@ class TestCourse(TestCase):
     def test_is_active(self):
         self.assertFalse(self.course.is_active)
 
-    def test_object_has_courseAttributes(self):
-        self.assertIsNotNone(self.course.course_attributes_id)
-
     def test_get_course_attributes_object(self):
         attrs = self.course.get_course_attributes_object()
 
         self.assertTrue(isinstance(attrs, CourseAttributes))
-        self.assertTrue(attrs.course_id, self.course.id)
-        self.assertTrue(attrs.course_title, "Tennis Lessons")
-
-        CourseAttributes.objects.get(id=self.course.course_attributes_id).update(custom=["123"])
-
-        attributes = self.course.get_course_attributes_object()
-        self.assertTrue(attributes.custom, ["123"])
+        self.assertEqual(attrs.course_id, self.course.id)
+        self.assertEqual(attrs.course_title, "Tennis Lessons")
 
     def tearDown(self):
         # Drop test DB and close connection
